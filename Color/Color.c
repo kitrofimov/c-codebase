517e53d72d22678888ff23eb39133999304aaa73
kirill <kitrofimov39@gmail.com>
2023-12-20 12:27:58 +0600

    Add colorMultiplyByScalar

--- Color/Color.c
+++ Color/Color.c
@@ -1,5 +1,15 @@
 #include "Color.h"
 
+Color colorMultiplyByScalar(Color this, double scalar)
+{
+    return (Color) {
+        (uint8_t) this.r * scalar, 
+        (uint8_t) this.g * scalar,
+        (uint8_t) this.b * scalar,
+        (uint8_t) this.a * scalar
+    }
+}
+
 Vector4d ColorToVector4d(Color color)
 {
     return (Vector4d) {
--- Color/Color.h
+++ Color/Color.h
@@ -8,5 +8,7 @@ typedef struct
 
 #include "Vector/Vector4.h"
 
+// Does not check if there is overflow or not
+Color colorMultiplyByScalar(double scalar);
 Vector4d ColorToVector4d(Color color);
 
