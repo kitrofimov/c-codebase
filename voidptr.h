9519eef21359e0d2144fef02af38901a0ad41c49
Kirill <kitrofimov39@gmail.com>
2024-05-08 22:25:33 +0500

    Add `voidptr.h`

---
+++ voidptr.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <stdint.h>
+
+#define INDEX_VOID_PTR(ptr, i, bytesPerElem) \
+    ((uint8_t*) (ptr) + (i) * (bytesPerElem))
+#define VOID_PTR_ADD(ptr, increment) \
+    ((uint8_t*) (ptr) + (increment))
+
